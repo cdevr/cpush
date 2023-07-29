e3b9bd2388b4bcf95a23e2b3a756a0e5bb78e20a
Christophe Devriese <christophe.devriese@gmail.com>
2023-07-29 16:08:33 +0200

    Add a test for RemovePromptSuffix

---
+++ cpush_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestRemovePromptSuffix(t *testing.T) {
+	tests := []struct{
+		Input string
+		Want string
+	}{
+		{ 
+			"output line\noutput line2\nprompt#",
+			"output line\noutput line2",
+		},
+		{
+			"just some output",
+			"just some output",
+		},
+	}
+
+	for _, test := range tests {
+		got := RemovePromptSuffix(test.Input)
+
+		if got != test.Want {
+			t.Errorf("RemovePromptSuffix(%q): got %q want %q", test.Input, got, test.Want)
+		}
+	}
+}
