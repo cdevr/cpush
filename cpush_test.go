2050a26a7639f4624a2141e11e023837e3b582d9
Christophe Devriese <christophe.devriese@gmail.com>
2023-08-03 16:19:19 +0200

    Another test

--- cpush_test.go
+++ cpush_test.go
@@ -27,12 +27,22 @@ func TestRemovePromptSuffix(t *testing.T) {
 }
 
 func TestSimpleThreadSafeBuffer(t *testing.T) {
+	testStr := "boembabies"
+
 	// Create a Buffer
 	b := ThreadSafeBuffer{}
 
-	b.Write([]byte("boembabies"))
+	if b.Len() != 0 {
+		t.Errorf("Unexpected buffer length before writing to ThreadSafeBuffer")
+	}
 
-	if b.String() != "boembabies" {
+	b.Write([]byte(testStr))
+
+	if b.Len() != len(testStr) {
+		t.Errorf("Unexpected buffer length after writing to ThreadSafeBuffer")
+	}
+
+	if b.String() != testStr {
 		t.Errorf("Can't write and read back from ThreadSafeBuffer")
 	}
 }
