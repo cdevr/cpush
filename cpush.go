c83e6807dbf59cf217dc848d5bc068f07b48442a
Christophe Devriese <christophe.devriese@gmail.com>
2023-09-27 17:01:58 +0200

    Initial version of interactive shell support. Implicit argument parsing is still broken.

--- cpush.go
+++ cpush.go
@@ -7,15 +7,16 @@ import (
 	"log"
 	"os"
 	"os/user"
-  "runtime"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/cdevr/cpush/cisco"
-	"github.com/cdevr/cpush/pwcache"
-	"github.com/cdevr/cpush/utils"
 	"github.com/cdevr/cpush/configfile"
+	"github.com/cdevr/cpush/pwcache"
+	"github.com/cdevr/cpush/shell"
+	"github.com/cdevr/cpush/utils"
 
 	"golang.org/x/net/proxy"
 )
@@ -26,8 +27,10 @@ var device = flag.String("device", "", "a device to execute commands on")
 var deviceFile = flag.String("devicefile", "", "file with a list of device to execute commands on. One device per line")
 var deviceStdIn = flag.Bool("devicestdin", false, "read list of devices from stdin (don't forget to CTRL-D, or provide EOF)")
 var deviceList = flag.String("devices", "", "comma-separated list of routers")
+
 var command = flag.String("cmd", "", "a command to execute")
 var push = flag.String("push", "", "something put into the configuration")
+var interactive = flag.Bool("i", false, "create an interactive shell on the device")
 
 var suppressBanner = flag.Bool("suppress_banner", true, "suppress the SSH banner and login")
 var suppressAdmin = flag.Bool("suppress_admin", true, "suppress administrative information")
@@ -67,7 +70,7 @@ type routerOutput struct {
 
 type routerError struct {
 	router string
-	err error
+	err    error
 }
 
 // CmdDevices executes a command on many devices, prints the output.
@@ -79,40 +82,40 @@ func CmdDevices(opts *cisco.Options, devices []string, username string, password
 
 	done := make(chan bool)
 
-  started := make(chan bool)
-  ended := make(chan bool)
+	started := make(chan bool)
+	ended := make(chan bool)
 
-  startCount := 0
-  endedCount := 0
+	startCount := 0
+	endedCount := 0
 
-  doDevice := func(device string) {
-    defer wg.Done()
-    started <- true
-    defer func() { ended <- true }()
+	doDevice := func(device string) {
+		defer wg.Done()
+		started <- true
+		defer func() { ended <- true }()
 
-    var output string
-    var err error
-    done := make(chan bool)
-    go func() {
-      output, err = cisco.Cmd(opts, device, username, password, cmd)
-      done <- true
-    }()
+		var output string
+		var err error
+		done := make(chan bool)
+		go func() {
+			output, err = cisco.Cmd(opts, device, username, password, cmd)
+			done <- true
+		}()
 
-    select {
-    case <-done:
-      if err != nil {
-        errors <- routerError{device, err}
-      } else {
-        outputs <- routerOutput{device, output}
-      }
-    case <-time.After(*timeout):
-      errors <- routerError{device, fmt.Errorf("router %q hit timeout after %v", device, *timeout)}
-    }
-  }
+		select {
+		case <-done:
+			if err != nil {
+				errors <- routerError{device, err}
+			} else {
+				outputs <- routerOutput{device, output}
+			}
+		case <-time.After(*timeout):
+			errors <- routerError{device, fmt.Errorf("router %q hit timeout after %v", device, *timeout)}
+		}
+	}
 
 	for _, d := range devices {
 		wg.Add(1)
-    go doDevice(d)
+		go doDevice(d)
 	}
 
 	go func() {
@@ -123,38 +126,38 @@ func CmdDevices(opts *cisco.Options, devices []string, username string, password
 	allDone := false
 	for !allDone {
 		select {
-    case <-started:
-      startCount += 1
-      fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
-    case <-ended:
-      startCount -= 1
-      endedCount += 1
-      fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
+		case <-started:
+			startCount += 1
+			fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
+		case <-ended:
+			startCount -= 1
+			endedCount += 1
+			fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
 		case re := <-errors:
 			fmt.Printf("\rerror on %q: %v\n", re.router, re.err)
 		case output := <-outputs:
-      if *logOutputTemplate != "" {
-        fn := strings.ReplaceAll(*logOutputTemplate, "%s", output.router)
-        err := utils.ReplaceFile(fn, utils.Dos2Unix(output.output))
-        if err != nil {
-          log.Printf("failed to save output for router %q: %v", output.router, err)
-        }
-      }
+			if *logOutputTemplate != "" {
+				fn := strings.ReplaceAll(*logOutputTemplate, "%s", output.router)
+				err := utils.ReplaceFile(fn, utils.Dos2Unix(output.output))
+				if err != nil {
+					log.Printf("failed to save output for router %q: %v", output.router, err)
+				}
+			}
 
 			lines := strings.Split(output.output, "\n")
 			for _, line := range lines {
-        if !*suppressOutput {
-          if *showDeviceName {
-            fmt.Printf("%s: %s\n", output.router, line)
-          } else {
-            fmt.Printf("%s\n", line)
-          }
-        }
+				if !*suppressOutput {
+					if *showDeviceName {
+						fmt.Printf("%s: %s\n", output.router, line)
+					} else {
+						fmt.Printf("%s\n", line)
+					}
+				}
 			}
 		case <-done:
 			allDone = true
-      fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
-      fmt.Printf("\n")
+			fmt.Printf("\033[2K\r%d/%d/%d", startCount, endedCount, len(devices))
+			fmt.Printf("\n")
 		}
 	}
 }
@@ -175,12 +178,12 @@ func main() {
 	configfile.ParseConfigFile("~/.cpush")
 	flag.Parse()
 
-  if *version {
-    fmt.Printf("cpush git revision %s compiled at %s with Go %s\n", buildGitRevision, buildTime, runtime.Version())
-    return
-  }
+	if *version {
+		fmt.Printf("cpush git revision %s compiled at %s with Go %s\n", buildGitRevision, buildTime, runtime.Version())
+		return
+	}
 
-	if flag.NArg()+flag.NFlag() == 0 {
+	if len(os.Args) == 1 {
 		fmt.Printf(`cpush tool to send commands to Cisco and Juniper routers
 	
 Simplest usage:
@@ -196,7 +199,9 @@ Other flags are:`)
 		return
 	}
 	// Allow device and command arguments to be passed in as non-args.
-	if *device == "" && *command == "" && flag.NArg() >= 2 {
+	if *device == "" && flag.NArg() == 1 {
+		*device = flag.Arg(0)
+	} else if *device == "" && *command == "" && flag.NArg() >= 2 {
 		*device = flag.Arg(0)
 		*command = strings.Join(flag.Args()[1:], " ")
 	}
@@ -204,7 +209,7 @@ Other flags are:`)
 	if *device == "" && *deviceList == "" && *deviceFile == "" {
 		*deviceStdIn = true
 	}
-	if *command == "" && *push == "" {
+	if *command == "" && *push == "" && *interactive == false {
 		log.Printf("you didn't pass in a command or a confliglet")
 		return
 	}
@@ -233,29 +238,34 @@ Other flags are:`)
 	}
 
 	if *device != "" {
-    var output string
-    if *command != "" {
-      output, err = cisco.Cmd(opts, *device, *username, password, *command)
-      if err != nil {
-        log.Fatalf("failed to execute command %q on device %q: %v", *command, *device, err)
-      }
-    }
-    if *push != "" {
-      output, err = cisco.Push(opts, *device, *username, password, *push)
-      if err != nil {
-        log.Fatalf("failed to commit configlet %q on device %q: %v", *command, *device, err)
-      }
-    }
-    if *logOutputTemplate != "" {
-      fn := strings.ReplaceAll(*logOutputTemplate, "%s", *device)
-      err := utils.AppendToFile(fn, utils.Dos2Unix(output))
-      if err != nil {
-        log.Printf("failed to save output for router %q: %v", *device, err)
-      }
-    }
-    if !*suppressOutput {
-      fmt.Printf("%s\n", output)
-    }
+		if *interactive {
+			log.Printf("starting interactive shell")
+			shell.Interactive(opts, *device, *username, password)
+			return
+		}
+		var output string
+		if *command != "" {
+			output, err = cisco.Cmd(opts, *device, *username, password, *command)
+			if err != nil {
+				log.Fatalf("failed to execute command %q on device %q: %v", *command, *device, err)
+			}
+		}
+		if *push != "" {
+			output, err = cisco.Push(opts, *device, *username, password, *push)
+			if err != nil {
+				log.Fatalf("failed to commit configlet %q on device %q: %v", *command, *device, err)
+			}
+		}
+		if *logOutputTemplate != "" {
+			fn := strings.ReplaceAll(*logOutputTemplate, "%s", *device)
+			err := utils.AppendToFile(fn, utils.Dos2Unix(output))
+			if err != nil {
+				log.Printf("failed to save output for router %q: %v", *device, err)
+			}
+		}
+		if !*suppressOutput {
+			fmt.Printf("%s\n", output)
+		}
 	} else if *deviceList != "" {
 		devices := strings.Split(*deviceList, ",")
 
