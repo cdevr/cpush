3451fbace91e699e1ab3db2d03ca2da1353e7e4a
Christophe Devriese <christophe.devriese@gmail.com>
2023-10-10 18:10:07 +0200

    Make go build and go install work correctly again.

---
+++ cmd/cpush/tags.go
@@ -0,0 +1,8 @@
+package main
+
+import "time"
+
+var (
+	buildTime = time.UnixMicro(1697061786568075)
+	buildGitRevision = "35abeaf430518a9bd37089ded6d0d9e85753662d"
+)
---
+++ cmd/rcheck/tags.go
@@ -0,0 +1,8 @@
+package main
+
+import "time"
+
+var (
+	buildTime = time.UnixMicro(1697061787145292)
+	buildGitRevision = "35abeaf430518a9bd37089ded6d0d9e85753662d"
+)
