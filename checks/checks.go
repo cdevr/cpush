65543be6a898a43831b5f801a88bdf1ae4bf90c8
Christophe Devriese <christophe.devriese@gmail.com>
2023-10-20 11:43:25 +0200

    Check BGP session state in rcheck. Needs to be tested.

--- checks/checks.go
+++ checks/checks.go
@@ -3,6 +3,7 @@ package checks
 import (
 	"fmt"
 	"github.com/cdevr/cpush/textfsm"
+	"strconv"
 )
 
 type CheckResult struct {
@@ -22,6 +23,11 @@ var Checks = []CheckData{
 		"Interfaces",
 		[]string{"show interfaces"},
 		CheckInterfaces,
+	},
+	{
+		"BGP sessions",
+		[]string{"show bgp sum"},
+		CheckBgpSum,
 	},
 }
 
@@ -48,6 +54,12 @@ func Check(device string, cmdResults map[string]string) ([]CheckResult, error) {
 func CheckInterfaces(router string, cmdResults map[string]string) ([]CheckResult, error) {
 	var results []CheckResult
 
+	checkName := "CheckInterfaces"
+
+	if _, ok := cmdResults["show interfaces"]; !ok {
+		return []CheckResult{{checkName, router, "failed to get 'show interfaces' command output"}}, nil
+	}
+
 	interfaceResults, err := textfsm.ParseTypedCiscoIosShowInterfaces(cmdResults["show interfaces"])
 	if err != nil {
 		return nil, fmt.Errorf("couldnt parse interfaces result")
@@ -59,30 +71,54 @@ func CheckInterfaces(router string, cmdResults map[string]string) ([]CheckResult
 		case ir.LinkStatus == "up" && ir.ProtocolStatus == "up":
 		case ir.LinkStatus == "administratively down" && ir.ProtocolStatus == "down":
 		default:
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: admin %q protocol %q", ir.Intf, ir.LinkStatus, ir.ProtocolStatus)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: admin %q protocol %q", ir.Intf, ir.LinkStatus, ir.ProtocolStatus)})
 		}
 
 		if ir.Runts != "" && ir.Runts != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s runts", ir.Intf, ir.Runts)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s runts", ir.Intf, ir.Runts)})
 		}
 		if ir.Giants != "" && ir.Giants != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s giants", ir.Intf, ir.Giants)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s giants", ir.Intf, ir.Giants)})
 		}
 		if ir.InputErrors != "" && ir.InputErrors != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s input errors", ir.Intf, ir.InputErrors)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s input errors", ir.Intf, ir.InputErrors)})
 		}
 		if ir.Crc != "" && ir.Crc != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s CRC errors", ir.Intf, ir.Crc)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s CRC errors", ir.Intf, ir.Crc)})
 		}
 		if ir.Overrun != "" && ir.Overrun != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s frame overruns", ir.Intf, ir.Overrun)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s frame overruns", ir.Intf, ir.Overrun)})
 		}
 		if ir.Abort != "" && ir.Abort != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s abort errors", ir.Intf, ir.Abort)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s abort errors", ir.Intf, ir.Abort)})
 		}
 		if ir.OutputErrors != "" && ir.OutputErrors != "0" {
-			results = append(results, CheckResult{"CheckInterfaces.IntfStatus", router, fmt.Sprintf("%s: %s output errors", ir.Intf, ir.OutputErrors)})
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: %s output errors", ir.Intf, ir.OutputErrors)})
 		}
 	}
+	return results, nil
+}
+
+func CheckBgpSum(router string, cmdResults map[string]string) ([]CheckResult, error) {
+	var results []CheckResult
+
+	checkName := "CheckBgpSum"
+
+	if _, ok := cmdResults["show bgp sum"]; !ok {
+		return []CheckResult{{checkName, router, "failed to get 'show bgp sum' command output"}}, nil
+	}
+
+	bgpSum, err := textfsm.ParseTypedCiscoIosShowBgpSummary(cmdResults["show bgp sum"])
+	if err != nil {
+		return nil, fmt.Errorf("couldnt parse show bgp sum result")
+	}
+
+	for _, neighbor := range bgpSum {
+		// Neighbor status should be the number of prefixes received. If it's anything else ("Idle", or "Connect", or "Active"), that's bad.
+		if _, err := strconv.Atoi(neighbor.Status); err == nil {
+			results = append(results, CheckResult{checkName, router, fmt.Sprintf("%s: idle status %q", neighbor.RemoteIp, neighbor.Status)})
+		}
+	}
+
 	return results, nil
 }
