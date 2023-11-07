eca572689ef99c130ae0bafafaf9c123db99cd39
Christophe Devriese <christophe.devriese@gmail.com>
2023-11-07 15:39:17 +0100

    Add precommit hook to properly tag files

--- cmd/rcheck/tags.go
+++ cmd/rcheck/tags.go
@@ -3,6 +3,6 @@ package main
 import "time"
 
 var (
-	buildTime = time.UnixMicro(1698330194342196)
-	buildGitRevision = "a4bdcb0adac388d9d2121929f81bb1d30dd45f5b"
+	buildTime = time.UnixMicro(1699367958216760)
+	buildGitRevision = "c84ecb38151f6ffae7b067e79b80bd363988150a"
 )
